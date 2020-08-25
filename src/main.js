fb49e871d37994a21d67b3d6978b86152ca4383d
ByungyeonKim <viva_kby@naver.com>
2020-08-25 19:40:48 +0900

    JS: display items

--- src/main.js
+++ src/main.js
@@ -1,17 +1,32 @@
 'use strict';
 
-// Fetch the items from the JSON file
+// 제이슨 파일에서 items fetch하기
 function loadItems() {
   return fetch('data/data.json')
     .then((response) => response.json())
     .then((json) => json.items);
 }
 
+// 주어진 items와 list 업데이트
+function displayItems(items) {
+  const container = document.querySelector('.items');
+  container.innerHTML = items.map((item) => createHTMLString(item)).join('');
+}
+
+// 주어진 데이타 item에서 HTML list 생성
+function createHTMLString(item) {
+  return `
+  <li class="item">
+     <img src="${item.image}" alt="${item.type}" class="item__thumbnail" />
+     <span class="item__description">${item.gender}, ${item.size}</span>
+  </li>
+  `;
+}
+
 // main
 loadItems()
   .then((items) => {
-    console.log(items);
-    // displayItems(items);
+    displayItems(items);
     // setEventListeners(items);
   })
   .catch(console.log);
